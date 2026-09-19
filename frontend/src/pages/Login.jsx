9b09b6938202975de69db434640a1653f7731068
senan <sanigull03@gmail.com>
2026-09-19 16:51:35 +0500

    Add mixBlendMode lighten to logo

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -90,7 +90,8 @@ const Login = () => {
                                 height: '85%',
                                 objectFit: 'contain',
                                 backgroundColor: 'transparent',
-                                filter: 'saturate(1.1) contrast(1.05)'
+                                filter: 'saturate(1.1) contrast(1.05)',
+                                mixBlendMode: 'lighten'
                             }}
                         />
                     </div>
